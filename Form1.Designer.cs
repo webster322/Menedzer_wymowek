778b088a2835b0bf8b85d7fa5277a01d0a07b147
Mateusz Kaczmarek <mk16939@gmail.com>
2017-07-29 16:03:46 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace MenedzerWymowek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.description = new System.Windows.Forms.TextBox();
+            this.results = new System.Windows.Forms.TextBox();
+            this.lastUsed = new System.Windows.Forms.DateTimePicker();
+            this.fileDate = new System.Windows.Forms.Label();
+            this.folder = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.open = new System.Windows.Forms.Button();
+            this.randomExcuse = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Wymówka";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 28);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Wyniki";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 50);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ostatnio użyte";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 71);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Data pliku";
+            // 
+            // description
+            // 
+            this.description.Location = new System.Drawing.Point(102, 5);
+            this.description.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(211, 20);
+            this.description.TabIndex = 4;
+            this.description.TextChanged += new System.EventHandler(this.description_TextChanged);
+            // 
+            // results
+            // 
+            this.results.Location = new System.Drawing.Point(102, 26);
+            this.results.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.results.Name = "results";
+            this.results.Size = new System.Drawing.Size(211, 20);
+            this.results.TabIndex = 5;
+            this.results.TextChanged += new System.EventHandler(this.results_TextChanged);
+            // 
+            // lastUsed
+            // 
+            this.lastUsed.Location = new System.Drawing.Point(102, 47);
+            this.lastUsed.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lastUsed.Name = "lastUsed";
+            this.lastUsed.Size = new System.Drawing.Size(203, 20);
+            this.lastUsed.TabIndex = 6;
+            this.lastUsed.ValueChanged += new System.EventHandler(this.lastUsed_ValueChanged);
+            // 
+            // fileDate
+            // 
+            this.fileDate.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.fileDate.Location = new System.Drawing.Point(102, 70);
+            this.fileDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.fileDate.Name = "fileDate";
+            this.fileDate.Size = new System.Drawing.Size(209, 15);
+            this.fileDate.TabIndex = 7;
+            // 
+            // folder
+            // 
+            this.folder.Location = new System.Drawing.Point(20, 94);
+            this.folder.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.folder.Name = "folder";
+            this.folder.Size = new System.Drawing.Size(60, 22);
+            this.folder.TabIndex = 8;
+            this.folder.Text = "Folder";
+            this.folder.UseVisualStyleBackColor = true;
+            this.folder.Click += new System.EventHandler(this.folder_Click);
+            // 
+            // save
+            // 
+            this.save.Enabled = false;
+            this.save.Location = new System.Drawing.Point(84, 94);
+            this.save.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(60, 22);
+            this.save.TabIndex = 9;
+            this.save.Text = "Zapisz";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // open
+            // 
+            this.open.Enabled = false;
+            this.open.Location = new System.Drawing.Point(148, 94);
+            this.open.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(60, 22);
+            this.open.TabIndex = 10;
+            this.open.Text = "Otwórz";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // randomExcuse
+            // 
+            this.randomExcuse.Enabled = false;
+            this.randomExcuse.Location = new System.Drawing.Point(212, 94);
+            this.randomExcuse.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.randomExcuse.Name = "randomExcuse";
+            this.randomExcuse.Size = new System.Drawing.Size(121, 22);
+            this.randomExcuse.TabIndex = 11;
+            this.randomExcuse.Text = "Losowa wymówka";
+            this.randomExcuse.UseVisualStyleBackColor = true;
+            this.randomExcuse.Click += new System.EventHandler(this.randomExcuse_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 125);
+            this.Controls.Add(this.randomExcuse);
+            this.Controls.Add(this.open);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.folder);
+            this.Controls.Add(this.fileDate);
+            this.Controls.Add(this.lastUsed);
+            this.Controls.Add(this.results);
+            this.Controls.Add(this.description);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Program do zarządzania wymówkami";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox description;
+        private System.Windows.Forms.TextBox results;
+        private System.Windows.Forms.DateTimePicker lastUsed;
+        private System.Windows.Forms.Label fileDate;
+        private System.Windows.Forms.Button folder;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.Button randomExcuse;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MenedzerWymowek
+{
+    using System.IO;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            currentExcuse.LastUsed = lastUsed.Value;
+        }
+
+        private void UpdateForm(bool changed)
+        {
+            if (!changed)
+            {
+                this.description.Text = currentExcuse.Description;
+                this.results.Text = currentExcuse.Results;
+                this.lastUsed.Value = currentExcuse.LastUsed;
+                if (!String.IsNullOrEmpty(currentExcuse.ExcusePath))
+                    fileDate.Text = File.GetLastWriteTime(currentExcuse.ExcusePath).ToString();
+                this.Text = "Program do zarządzania wymówkami";
+            }
+            else
+                this.Text = "Program do zarządzania wymówkami*";
+            this.formChanged = changed;
+        }
+
+        private Excuse currentExcuse = new Excuse();
+        private string selectedFolder = "";
+        private bool formChanged = false;
+        Random random = new Random();
+
+        private void folder_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = selectedFolder;
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                selectedFolder = folderBrowserDialog1.SelectedPath;
+                save.Enabled = true;
+                open.Enabled = true;
+                randomExcuse.Enabled = true;
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(description.Text) || String.IsNullOrEmpty(results.Text))
+            {
+                MessageBox.Show("Określ wymówkę i rezultat",
+                     "Nie można zapisać pliku", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            saveFileDialog1.InitialDirectory = selectedFolder;
+            saveFileDialog1.Filter = "Pliki tekstowe (*.txt)|*.txt|Wszystkie pliki (*.*)|*.*";
+            saveFileDialog1.FileName = description.Text + ".txt";
+            DialogResult result = saveFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                currentExcuse.Save(saveFileDialog1.FileName);
+                UpdateForm(false);
+                MessageBox.Show("Wymówka została zapisana");
+            }
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            if (CheckChanged())
+            {
+                openFileDialog1.InitialDirectory = selectedFolder;
+                openFileDialog1.Filter = "Pliki tekstowe (*.txt)|*.txt|Wszystkie pliki (*.*)|*.*";
+                openFileDialog1.FileName = description.Text + ".txt";
+                DialogResult result = openFileDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    currentExcuse = new Excuse(openFileDialog1.FileName);
+                    UpdateForm(false);
+                }
+            }
+        }
+
+        private void randomExcuse_Click(object sender, EventArgs e)
+        {
+            if (CheckChanged())
+            {
+                currentExcuse = new Excuse(random, selectedFolder);
+                UpdateForm(false);
+            }
+        }
+
+        private bool CheckChanged()
+        {
+            if (formChanged)
+            {
+                DialogResult result = MessageBox.Show(
+                      "Bieżąca wymówka nie została zapisana. Czy kontynuować?",
+                       "Ostrzeżenie", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.No)
+                    return false;
+            }
+            return true;
+        }
+
+        private void description_TextChanged(object sender, EventArgs e)
+        {
+            currentExcuse.Description = description.Text;
+            UpdateForm(true);
+        }
+
+        private void results_TextChanged(object sender, EventArgs e)
+        {
+            currentExcuse.Results = results.Text;
+            UpdateForm(true);
+        }
+
+        private void lastUsed_ValueChanged(object sender, EventArgs e)
+        {
+            currentExcuse.LastUsed = lastUsed.Value;
+            UpdateForm(true);
+        }
+    }
+}
